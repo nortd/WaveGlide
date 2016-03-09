39b4e4d3313fa9c209c4e3a59ce1d10f371707a1
Stefan Hechenberger <stefan@nortd.com>
2016-03-09 01:04:16 +0100

    flat breath opti

--- src/firmware/rhythm.c
+++ src/firmware/rhythm.c
@@ -22,14 +22,15 @@
 int vals[RHYTHM_VALS_SIZE] = {0};
 uint8_t vals_cursor = 0;
 
-#define RHYTHM_BASELINE_TOL 3
+#define RHYTHM_BASELINE_TOL_LOW 2
+#define RHYTHM_BASELINE_TOL_HIGH 8
 #define RHYTHM_BASELINE_DUR 1000/RHYTHM_TEMPRES  // dur for new baseline, 1s
 #define RHYTHM_OFFLINE_DUR 4000/RHYTHM_TEMPRES   // time considered as canula off event
 int baseline = 512;  // baseline sensor read (when not breathing)
 int baseline_last_ref = 512;
 bool baseline_ref_set = false;
 uint16_t baseline_flat_dur = 0;
-uint8_t baseline_last_tol = RHYTHM_BASELINE_TOL;
+uint8_t baseline_last_tol = RHYTHM_BASELINE_TOL_LOW;
 
 #define RHYTHM_OFFSET_PCT 0.03   // trigger feature to inhalation start pct
 #define RHYTHM_INHALE_PCT 0.32    // period percentage useful for oxygenation
@@ -53,17 +54,19 @@ void rhythm_addval(int val) {
       && val >= (baseline_last_ref - baseline_last_tol)) {
     // val around baseline_last_ref
     baseline_flat_dur++;
+    if (baseline_flat_dur > 24) {
+      // reduce sensitivity
+      baseline_last_tol = RHYTHM_BASELINE_TOL_HIGH;
+    }
     if (!baseline_ref_set && (baseline_flat_dur == RHYTHM_BASELINE_DUR)) {
       // got new baseline
       baseline = baseline_last_ref;
       baseline_ref_set = true;
-      // increase baseline tolerance
-      // prevents mis-oxygenation from sensor noise
-      baseline_last_tol += 2;
     }
   } else {
     baseline_flat_dur = 0;
     baseline_last_ref = val;
+    baseline_last_tol = RHYTHM_BASELINE_TOL_LOW;
   }
 
   // add val to vals
